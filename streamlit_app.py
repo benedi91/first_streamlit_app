da8a2add51a4cd99166c2f07c23f3a184aaebc2f
Tomasz B <46606631+benedi91@users.noreply.github.com>
2022-06-10 10:30:49 +0200

    Update streamlit_app.py
    added fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.title('My Parents New Healthly Dinner')
 
 streamlit.header('Breakfast Favorites')
@@ -11,5 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# fruit picker
+streamlight.multiselect("Pick some fruits:",list(my_fruit_list.index))
+# display table on the page
 streamlit.dataframe(my_fruit_list)
