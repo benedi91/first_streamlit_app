e983fe1f221061e7ecb8d3b017ec4a80fb294799
Tomasz B <46606631+benedi91@users.noreply.github.com>
2022-06-14 11:48:24 +0200

    add a button and function to snowflake section

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,19 +44,26 @@ try:
 except URLError as e:
   streamlit.error()
   
-# dont run anything past here while we troubleshoot
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)    
+#Snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur
+  my_cur.execute("SELECT * from fruit_load_list")
+  return my_cur.fetchall()
 
-#Allow end used add fruit to list
+#add button to load the fruit:
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)    
+
+#Allow end user add fruit to list
 add_my_fruit = streamlit.text_input('what fruit would you like informaion about?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
 #will not work correctly
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+# dont run anything past here while we troubleshoot
+streamlit.stop()
